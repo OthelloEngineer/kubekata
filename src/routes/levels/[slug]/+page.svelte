e00b431e1b9eb72c5d831e82abc01c5a2cbc87a7
TidosDK <git@msnn.dev>
2024-09-11 10:39:01 +0200

    Fixed rebase merge conflict

--- src/routes/levels/[slug]/+page.svelte
+++ src/routes/levels/[slug]/+page.svelte
@@ -10,7 +10,6 @@
     import CutePicture from "./CutePicture.svelte";
     let name = $page.params.slug;
     let levels = levelExample.flatMap((level) => level.levels);
-    console.log(levels);
     let level = levels.find((level) => level.title === name);
     let isComplete = level?.isCompleted;
     let description = level?.description;
--- src/routes/levels/[slug]/CutePicture.svelte
+++ src/routes/levels/[slug]/CutePicture.svelte
@@ -2,6 +2,6 @@
     export let svgName: string;
 </script>
 
-<div class="max-h-5">
-    <img src={svgName} alt="Cute" class="object-cover" />
+<div class="w-full flex items-center justify-center overflow-hidden">
+    <img src={svgName} alt="Cute" class="h-full w-auto object-cover max-h-96 pt-2"/>
 </div>
