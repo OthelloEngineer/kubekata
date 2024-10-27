9682c45f752149f628d09813f95a2cc3f88a7df2
TidosDK <git@msnn.dev>
2024-10-27 03:04:08 +0100

    Fixed Level selection screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,27 +2,6 @@
     import "../app.css";
 </script>
 
-<div class="animated-background">
+<div>
     <slot></slot>
 </div>
-
-<style lang="postcss">
-    .animated-background {
-        @apply bg-gradient-to-b from-blue-800 via-blue-500 to-blue-800;
-        background-size: 120% 120%;
-        height: 100%;
-        width: 100%;
-        animation: animation 3s ease-in-out infinite;
-    }
-
-    @keyframes animation {
-        0%,
-        100% {
-            background-position: 50% 0%;
-        }
-
-        50% {
-            background-position: 50% 100%;
-        }
-    }
-</style>
--- src/routes/levels/+page.svelte
+++ src/routes/levels/+page.svelte
@@ -3,7 +3,6 @@
     import DownSwoop from "$lib/components/downSwoop.svelte";
     import kubecuddle from "$lib/icons/kubecuddle.png";
     import levelExample from "./levels-example.json";
-    import {MasonryGrid} from "@egjs/svelte-grid";
 
     let height = 20;
     let files: FileList;
@@ -16,11 +15,12 @@
     const column = 0;
     const columnSize = 0;
     const columnSizeRatio = 0;
-    levelExample.forEach((superLevel) => {
-        superLevel.levels.forEach((level) => {
-            console.log("level: ", level);
-        });
-    });
+    // DEBUG:
+    // levelExample.forEach((superLevel) => {
+    //     superLevel.levels.forEach((level) => {
+    //         console.log("level: ", level);
+    //     });
+    // });
     function print_file(): void {
         let config = files[0].text().then((text) => {
             let url = "/api"
@@ -28,8 +28,7 @@
             fetch(url, {
                 method: 'POST',
                 body: text.toString(),
-                headers: {
-                }
+                headers: {}
             }).then((res) => {
                 console.log("res: ", res)
                 if (res.status !== 200) {
@@ -43,57 +42,55 @@
     }
 </script>
 <div class="h-screen">
-<nav
-    class="
-        py-2 px-8 h-{height}
-        flex flex-row justify-between items-center
-        text-white
-        bg-gradient-to-b
-        from-navBar via-navBar via-90% to-opacity
-   "
->
-    <h1 class="text-4xl">Levels</h1>
-    <div class="flex flex-row space-x-4 items-center justify-center bg-gray-200 p-2 rounded-md bg-opacity-50">
-        <span> Current Mode: {currentMode} </span>
-    </div>
-    <div class="flex flex-row-reverse h-full">
-        <img class="object-contain" src={kubecuddle} alt="KubeCuddleFish" />
-    </div>
-    <div> 
-    <span> Switch to "config" Mode </span>
-
-
-    {#if hasSuccessfullyUploaded}
-        <span class="text-green-500, font-bold, bg-green-300, p-2, rounded-md, text-center, w-48, h-12, flex, items-center, justify-center"
-        > successfully uploaded </span>
-    {:else}
-        {#if files !== undefined}
-            <button on:click={print_file}> confirm upload </button>
-        {:else }
-            <input bind:files type="file"  />
-        {/if}  
-    {/if}
-    </div>
-    <div class="flex flex-row space-x-8 items-center">
-        <button> reset progress </button>
-        <button> delete all kata resources </button>
-    </div>
-</nav>
-<div class="flex flex-row space-x-4 mx-4 justify-between">
-    {#each levelExample as superLevel}
-    <div class="bg-green-200 bg-opacity-30 outline-3 outline-dashed outline-offset-2 w-fit outline-white rounded-md p-4 my-4">
-            <div class="flex flex-col place-items-center">
-                <h1 class="text-xl mb-5">{superLevel.name}</h1>
-                {#each superLevel.levels as level, i}
-                    <LevelButton name={level.title} isCompleted={level.isCompleted} />
-                    {#if i % 2 === 0 && i != superLevel.levels.length - 1}
-                        <DownSwoop orientation="right" />
-                    {:else if i % 2 === 1 && i != superLevel.levels.length - 1}
-                        <DownSwoop orientation="left" />
-                    {/if}
-                {/each}
-            </div>
+    <nav
+        class="
+            py-2 px-8 h-{height}
+            flex flex-row justify-between items-center
+            text-white
+            bg-gradient-to-b
+            from-navBar via-navBar via-90% to-opacity
+            ">
+        <h1 class="text-4xl">Levels</h1>
+        <div class="flex flex-row space-x-4 items-center justify-center bg-gray-200 p-2 rounded-md bg-opacity-50">
+            <span> Current Mode: {currentMode} </span>
         </div>
-    {/each}
-</div>
+        <div class="flex flex-row-reverse h-full">
+            <img class="object-contain" src={kubecuddle} alt="KubeCuddleFish" />
+        </div>
+        <div>
+            <span> Switch to "config" Mode </span>
+
+            {#if hasSuccessfullyUploaded}
+        <span class="text-green-500 font-bold bg-green-300 p-2 rounded-md text-center w-48 h-12 flex items-center justify-center"
+        > successfully uploaded </span>
+        {:else}
+            {#if files !== undefined}
+                <button on:click={print_file}> confirm upload </button>
+            {:else }
+                <input bind:files type="file"  />
+            {/if}
+        {/if}
+        </div>
+        <div class="flex flex-row space-x-8 items-center">
+            <button> reset progress </button>
+            <button> delete all kata resources </button>
+        </div>
+    </nav>
+    <div style="padding-left: clamp(0px, 20px, 20px); padding-right: clamp(0px, 20px, 20px);" class="flex flex-wrap gap-4 mx-auto justify-start">
+        {#each levelExample as superLevel}
+        <div style="margin-left: clamp(5px, 5vw, 30px); margin-right: clamp(5px, 5vw, 30px)" class="bg-green-200 p-4 bg-opacity-30 outline-3 outline-dashed outline-offset-2 min-w-[250px] max-w-[250px] flex-grow outline-white rounded-md my-4">
+                <div class="flex flex-col place-items-center">
+                    <h1 class="text-xl mb-5">{superLevel.name}</h1>
+                    {#each superLevel.levels as level, i}
+                        <LevelButton name={level.title} isCompleted={level.isCompleted} />
+                        {#if i % 2 === 0 && i != superLevel.levels.length - 1}
+                            <DownSwoop orientation="right" />
+                        {:else if i % 2 === 1 && i != superLevel.levels.length - 1}
+                            <DownSwoop orientation="left" />
+                        {/if}
+                    {/each}
+                </div>
+            </div>
+        {/each}
+    </div>
 </div>
