c3a28f4bf45cc460a9356318096a737a2d1b8669
TidosDK <git@msnn.dev>
2024-10-30 17:09:38 +0100

    Added setup shell script

--- ci/setup.sh
+++ ci/setup.sh
@@ -7,4 +7,30 @@ if [ "$CURRENT_FOLDER" = "ci" ]; then
     cd ..
 fi
 
-kubectl create clusterrolebinding default-view --clusterrole=view --serviceaccount=default:default
+CHECK_IF_MINIKUBE_IS_CURRENT_CONTEXT=$(kubectl config view | grep "current-context: minikube" | wc -l)
+
+if [ "$CHECK_IF_MINIKUBE_IS_CURRENT_CONTEXT" -lt 1 ]; then
+    if kubectl config get-contexts | grep -q "minikube"; then
+        echo "Switching to minikube context."
+        kubectl config use-context minikube
+    else
+        echo "Context minikube does not exist. Attempting to start minikube."
+        minikube start
+
+        if kubectl config get-contexts | grep -q "minikube"; then
+            echo "Switching to minikube context."
+            kubectl config use-context minikube
+        else
+            echo "Failed to start minikube."
+            exit 1
+        fi
+    fi
+fi
+
+CHECK_IF_CLUSTER_ROLE_BINDING_EXISTS=$(kubectl get clusterrolebinding | grep -c "default-view")
+
+if [ "$CHECK_IF_CLUSTER_ROLE_BINDING_EXISTS" -lt 1 ]; then
+    kubectl create clusterrolebinding default-view --clusterrole=view --serviceaccount=default:default
+fi
+
+echo "Setup finished."
